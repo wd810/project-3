b2f79dac574ed76d7d570d2b8f6ff06a01cab724
wangd <36665764+wangdianyh@users.noreply.github.com>
2019-04-11 15:51:26 -0400

    deploy test 2

--- project-3/static/js/predict.js
+++ project-3/static/js/predict.js
@@ -63,16 +63,13 @@ app.controller('myCtrl', function($scope, $http) {
                     $http.post('/pokemon-go', Indata).then(function(response) {
                         data = response.data;
                         console.log(data);
-                        //var lose_rate = Math.round((100 - data.win_rate) * 100) / 100;
 
                         if ($scope.pok_1_s.id == data.win_predict) {
                             $('#pok_1_box').addClass('winner-box');
                             $scope.pok_1_s.win_rate = 'Win Rate: ' + data.win_rate + '%';
-                            //$scope.pok_2_s.win_rate = 'Win Rate: ' + lose_rate + '%';
                         } else {
                             $('#pok_2_box').addClass('winner-box');
                             $scope.pok_2_s.win_rate = 'Win Rate: ' + data.win_rate + '%';
-                            //$scope.pok_1_s.win_rate = 'Win Rate: ' + lose_rate + '%';
                         }
                         //$scope.win_pok = data.win_predict;
                         $('#play-again').show();
